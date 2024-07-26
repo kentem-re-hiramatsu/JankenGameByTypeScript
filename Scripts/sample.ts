d0eb0fa2a17bb4c4f8bd8aae9f3273bca1475bd8
re-hiramatsu <re-hiramatsu>
2024-07-26 12:52:49 +0900

    じゃんけん判別処理実装

---
+++ Scripts/sample.ts
@@ -0,0 +1,104 @@
+"use strict";
+{
+    class Game {
+        private img: HTMLImageElement;
+        private timeoutId: NodeJS.Timeout | undefined = undefined;
+
+        constructor() {
+            const main: HTMLElement | null = document.querySelector('main');
+            const cpuSection: HTMLElement | null = document.getElementById('cpuHand');
+            const jankenResult: HTMLElement | null = document.getElementById('result');
+
+            this.img = document.createElement('img');
+            this.img.src = this.GetCpuHand();
+            this.img.width = 220;
+            this.img.height = 220;
+
+            cpuSection?.appendChild(this.img);
+
+            const game: HTMLDivElement = document.createElement('div');
+            game.className = 'game';
+
+            const startButton: HTMLDivElement = document.createElement('div');
+            startButton.id = 'gameStartButton';
+            startButton.textContent = 'Start';
+            startButton.addEventListener('click', () => {
+                this.SpinCpuHand();
+            });
+
+            const matchButton: HTMLDivElement = document.createElement('div');
+            matchButton.id = 'matchButton';
+            matchButton.textContent = '勝負!!';
+            const h2: HTMLElement = document.createElement('h2');
+
+            matchButton.addEventListener('click', () => {
+                clearTimeout(this.timeoutId);
+                h2.textContent = this.CheckHand();
+                jankenResult?.appendChild(h2);
+            });
+
+            game.appendChild(startButton);
+            game.appendChild(matchButton);
+            main?.appendChild(game);
+        }
+
+        GetCpuHand(): string {
+            const rockSrc: string = "../img/janken_gu.png";
+            const scissorsSrc: string = "../img/janken_choki.png";
+            const paperSrc: string = "../img/janken_pa.png";
+            const jankens: readonly string[] = [rockSrc, scissorsSrc, paperSrc];
+
+            return jankens[Math.floor(Math.random() * jankens.length)];
+        }
+
+        SpinCpuHand(): void {
+            this.img.src = this.GetCpuHand();
+            this.timeoutId = setTimeout(() => {
+                this.SpinCpuHand();
+            }, 100);
+        }
+
+        CheckHand(): string {
+            const jankens = document.getElementsByName('janken') as NodeListOf<HTMLInputElement>;
+            let userResult: number = -1;
+            let cpuResult: number;
+
+            jankens.forEach((janken) => {
+                if (janken.checked) {
+                    userResult = (Number)(janken.value);
+                }
+            });
+
+            console.log(userResult);
+
+            if (this.img.src.includes('janken_gu.png')) {
+                cpuResult = 0;
+            }
+            else if (this.img.src.includes('janken_choki.png')) {
+                cpuResult = 1;
+            }
+            else {
+                cpuResult = 2;
+            }
+
+            console.log(cpuResult);
+
+            let jankenResultNum: Number = (userResult - cpuResult + 3) % 3;
+            let jankenResult: string;
+
+            if (jankenResultNum === 0) {
+                jankenResult = 'DRAW';
+            }
+            else if (jankenResultNum === 2) {
+                jankenResult = 'WIN';
+            }
+            else {
+                jankenResult = 'LOSE';
+            }
+
+            return jankenResult;
+        }
+    }
+
+    new Game();
+}
---
+++ wwwroot/js/sample.js
@@ -0,0 +1,84 @@
+"use strict";
+{
+    class Game {
+        constructor() {
+            this.timeoutId = undefined;
+            const main = document.querySelector('main');
+            const cpuSection = document.getElementById('cpuHand');
+            const jankenResult = document.getElementById('result');
+            this.img = document.createElement('img');
+            this.img.src = this.GetCpuHand();
+            this.img.width = 220;
+            this.img.height = 220;
+            cpuSection === null || cpuSection === void 0 ? void 0 : cpuSection.appendChild(this.img);
+            const game = document.createElement('div');
+            game.className = 'game';
+            const startButton = document.createElement('div');
+            startButton.id = 'gameStartButton';
+            startButton.textContent = 'Start';
+            startButton.addEventListener('click', () => {
+                this.SpinCpuHand();
+            });
+            const matchButton = document.createElement('div');
+            matchButton.id = 'matchButton';
+            matchButton.textContent = '勝負!!';
+            const h2 = document.createElement('h2');
+            matchButton.addEventListener('click', () => {
+                clearTimeout(this.timeoutId);
+                h2.textContent = this.CheckHand();
+                jankenResult === null || jankenResult === void 0 ? void 0 : jankenResult.appendChild(h2);
+            });
+            game.appendChild(startButton);
+            game.appendChild(matchButton);
+            main === null || main === void 0 ? void 0 : main.appendChild(game);
+        }
+        GetCpuHand() {
+            const rockSrc = "../img/janken_gu.png";
+            const scissorsSrc = "../img/janken_choki.png";
+            const paperSrc = "../img/janken_pa.png";
+            const jankens = [rockSrc, scissorsSrc, paperSrc];
+            return jankens[Math.floor(Math.random() * jankens.length)];
+        }
+        SpinCpuHand() {
+            this.img.src = this.GetCpuHand();
+            this.timeoutId = setTimeout(() => {
+                this.SpinCpuHand();
+            }, 100);
+        }
+        CheckHand() {
+            const jankens = document.getElementsByName('janken');
+            let userResult = -1;
+            let cpuResult;
+            jankens.forEach((janken) => {
+                if (janken.checked) {
+                    userResult = (Number)(janken.value);
+                }
+            });
+            console.log(userResult);
+            if (this.img.src.includes('janken_gu.png')) {
+                cpuResult = 0;
+            }
+            else if (this.img.src.includes('janken_choki.png')) {
+                cpuResult = 1;
+            }
+            else {
+                cpuResult = 2;
+            }
+            console.log(cpuResult);
+            let jankenResultNum = (userResult - cpuResult + 3) % 3;
+            let jankenResult;
+            if (jankenResultNum === 0) {
+                jankenResult = 'DRAW';
+            }
+            else if (jankenResultNum === 2) {
+                jankenResult = 'WIN';
+            }
+            else {
+                jankenResult = 'LOSE';
+            }
+            return jankenResult;
+        }
+    }
+    new Game();
+}
+//# sourceMappingURL=sample.js.map
