4153bc3c75a9a4d2a4324a5a5f65c1214900cd92
re-hiramatsu <re-hiramatsu>
2024-07-29 16:14:52 +0900

    じゃんけんのenum実装

---
+++ Scripts/Enums/handType.ts
@@ -0,0 +1,7 @@
+"use strict";
+
+export enum HandType{
+    Gu = 0,
+    Choki = 1,
+    Pa = 2,
+}
---
+++ wwwroot/js/Enums/handType.js
@@ -0,0 +1,8 @@
+"use strict";
+export var HandType;
+(function (HandType) {
+    HandType[HandType["Gu"] = 0] = "Gu";
+    HandType[HandType["Choki"] = 1] = "Choki";
+    HandType[HandType["Pa"] = 2] = "Pa";
+})(HandType || (HandType = {}));
+//# sourceMappingURL=handType.js.map
