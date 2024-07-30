5e5d4f181cc3135f0313bb7a0791675b9192d0c9
re-hiramatsu <re-hiramatsu>
2024-07-30 13:21:47 +0900

    gameDataクラス実装

--- wwwroot/js/Models/gameData.js
+++ wwwroot/js/Models/gameData.js
@@ -1,3 +1,11 @@
 "use strict";
-export {};
+export class GameData {
+    constructor(gameCount, user, cpu, result, winRate) {
+        this._gameCount = gameCount;
+        this._userHand = user;
+        this._cpuHand = cpu;
+        this._result = result;
+        this._winRate = winRate;
+    }
+}
 //# sourceMappingURL=gameData.js.map
