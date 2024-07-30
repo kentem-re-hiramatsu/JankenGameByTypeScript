b2a5a54ff87f92d5970dff7f3491d6a951b42575
re-hiramatsu <re-hiramatsu>
2024-07-30 13:22:22 +0900

    jankenScoreクラス実装

--- Scripts/Models/jankenScore.ts
+++ Scripts/Models/jankenScore.ts
@@ -1,21 +1,22 @@
 "use strict";
 
-export class JankenScore{
-    private _winScore:number = 0;
-    private _totalScore:number = 0;
+export class JankenScore {
+    private _winScore: number = 0;
+    private _gameCount: number = 0;
 
-    public set Score(isWin:boolean){
-        if(isWin){
+    public set Score(isWin: boolean) {
+        if (isWin) {
             this._winScore++;
         }
-        this._totalScore++;
+        this._gameCount++;
     }
 
-    public get WinScore():number{
+    public get WinScore(): number {
         return this._winScore;
     }
 
-    public get TotalScore():number{
-        return this._totalScore;
+    public get WinRate(): number {
+        const winRate = Math.round(this._winScore / this._gameCount * 100) * 1;
+        return winRate;
     }
 }
--- wwwroot/js/Models/jankenScore.js
+++ wwwroot/js/Models/jankenScore.js
@@ -2,19 +2,20 @@
 export class JankenScore {
     constructor() {
         this._winScore = 0;
-        this._totalScore = 0;
+        this._gameCount = 0;
     }
     set Score(isWin) {
         if (isWin) {
             this._winScore++;
         }
-        this._totalScore++;
+        this._gameCount++;
     }
     get WinScore() {
         return this._winScore;
     }
-    get TotalScore() {
-        return this._totalScore;
+    get WinRate() {
+        const winRate = Math.round(this._winScore / this._gameCount * 100) * 1;
+        return winRate;
     }
 }
 //# sourceMappingURL=jankenScore.js.map
